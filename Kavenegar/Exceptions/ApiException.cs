5ce5d7987249747fe033c31a2fc4c72bfba665ac
Aryan <aryanebrahimpour@gmail.com>
2018-07-30 03:40:21 +0430

    Cleaner exceptions

--- Kavenegar/Exceptions/ApiException.cs
+++ Kavenegar/Exceptions/ApiException.cs
@@ -2,19 +2,16 @@ using Kavenegar.Models.Enums;
 
 namespace Kavenegar.Exceptions
 {
- public class ApiException : KavenegarException
- {
-	readonly MetaCode _result;
-	public ApiException(string message, int code)
-	 : base(message)
-	{
-	 _result = (MetaCode)code;
-	}
+    public class ApiException : KavenegarException
+    {
+        readonly MetaCode _result;
+        public ApiException(string message, int code)
+         : base(message)
+        {
+            _result = (MetaCode)code;
+        }
 
-	public MetaCode Code
-	{
-	 get { return _result; }
-	}
+        public MetaCode Code => _result;
 
- }
+    }
 }
--- Kavenegar/Exceptions/BaseException.cs
+++ Kavenegar/Exceptions/BaseException.cs
@@ -1,10 +1,10 @@
 namespace Kavenegar.Exceptions
 {
- public class KavenegarException : System.Exception
- {
-	public KavenegarException(string message)
-	 : base(message)
-	{
-	}
- }
+    public class KavenegarException : System.Exception
+    {
+        public KavenegarException(string message)
+         : base(message)
+        {
+        }
+    }
 }
